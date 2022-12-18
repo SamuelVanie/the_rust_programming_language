403f3593b5dcfc8c81d5afbdf0a0f770eac42b2a
Samuel Michael Vanié <bi-misanze-samuel-mi.vanie@grenoble-inp.org>
2022-12-18 13:43:15 +0100

    add explication comments in guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,22 +1,22 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::io; // because io functions aren't in the prelude we have to bring them
 
 fn main() {
     println!("Guess the number !");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100); // immutable value (default)
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess."); // println! is a macro use to print on stdin
 
-        let mut guess = String::new();
+        let mut guess = String::new(); // mutable value
 
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            .read_line(&mut guess) // where to use the user input. & mean it's passing by reference
+            .expect("Failed to read line"); // handling potential error
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() { // match return a Result type which is either Ok or Err
             Ok(num) => num,
             Err(_) => continue,
         };
