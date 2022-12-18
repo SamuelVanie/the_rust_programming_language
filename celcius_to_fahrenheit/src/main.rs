01670078c2290d6b4beb1ae2025874dca0091fea
Samuel Michael Vanié <bi-misanze-samuel-mi.vanie@grenoble-inp.org>
2022-12-18 13:55:30 +0100

    feat: add conversion from fahrenheit to celcius

--- celcius_to_fahrenheit/src/main.rs
+++ celcius_to_fahrenheit/src/main.rs
@@ -1,17 +1,44 @@
 use std::io;
 
 fn main() {
-    println!("Entrez la valeur en celcius:");
-    let mut celcius_val = String::new();
 
+    println!("Conversion de Celcius en Fahrenheit :");
+    println!("1. From celcius to Fahrenheit");
+    println!("2. From Fahrenheit to Celcius");
 
-    io::stdin()
-        .read_line(&mut celcius_val)
-        .expect("Valeur incorrecte");
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).expect("Entrez une valeur svp");
 
-    let celcius_val: u32 = celcius_val.trim().parse().expect("Impossible de convertir");
+    let choice = choice.trim();
 
-    let farenheit_value = (celcius_val * 9/5) + 32;
-    println!("la valeur en fahrenheit est : {farenheit_value}");
+    if choice == "1" {
+        println!("Entrez la valeur en celcius:");
+        let mut celcius_val = String::new();
+
+        io::stdin()
+            .read_line(&mut celcius_val)
+            .expect("Valeur incorrecte");
+
+        let celcius_val: f32 = celcius_val.trim().parse().expect("Impossible de convertir");
+
+        let farenheit_value = (celcius_val * 9.0/5.0) + 32.0;
+        println!("la valeur en fahrenheit est : {farenheit_value}");
+
+    } else if choice == "2" {
+
+        println!("Entrez la valeur en fahrenheit:");
+        let mut fahrenheit_val = String::new();
+
+        io::stdin()
+            .read_line(&mut fahrenheit_val)
+            .expect("Valeur incorrecte");
+
+        let fahrenheit_val: f32 = fahrenheit_val.trim().parse().expect("Impossible de convertir");
+
+        let celcius_val = (fahrenheit_val - 32.0) * 5.0/9.0;
+        println!("la valeur en fahrenheit est : {celcius_val}");
+
+    }
+
 
 }
